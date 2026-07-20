86a2ddff4ffa9b702f5dcfb23fd5980b2943972f
Nicocastellanos <nicolas.castellanos012@gmail.com>
2026-07-20 11:00:29 -0500

    merge 1

--- pfgame/src/pages/Home.tsx
+++ pfgame/src/pages/Home.tsx
@@ -1,9 +1,15 @@
-
+import "../css/Home.css"
 
 function Home() {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-container">
+        <h1 className="home-title">Welcome to My Portfolio</h1>
+        <h2 className="option-1">Projects</h2>
+        <h2 className="option-2">About Me</h2>
+        <h2 className="option-3">About Me</h2>
+      </div>
+  </section>
+)}
 
 export default Home
